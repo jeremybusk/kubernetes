f86e8248bf52ee3a1074651a9f20ccfc12b21325
Jeremy Busk <jeremy@pyrofex.net>
2018-09-14 17:27:05 -0600

    Update install-ubuntu18.04.sh

--- install-ubuntu18.04.sh
+++ install-ubuntu18.04.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+# modified from https://computingforgeeks.com/how-to-install-minikube-on-ubuntu-18-04/
+# This is base Ubuntu 18.04 Server install
+# This utilizes kvm2 driver
+
 sudo apt-get update
 sudo apt-get -y install apt-transport-https
 sudo apt-get -y upgrade
-#sudo apt install virtualbox virtualbox-ext-pack
+#sudo apt install virtualbox virtualbox-ext-pack # this is for virtuabox
 sudo apt install libvirt-clients libvirt-daemon-system qemu-kvm
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -O  /usr/local/bin/minikube
 chmod +x /usr/local/bin/minikube
@@ -14,7 +19,7 @@ sudo apt-get install -y kubectl
 
 kubectl version -o json
 
-#minikube start
+#minikube start # this is for virtualbox
 curl -Lo docker-machine-driver-kvm2 https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 \
 && chmod +x docker-machine-driver-kvm2 \
 && sudo cp docker-machine-driver-kvm2 /usr/local/bin/ \
